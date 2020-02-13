169689885fb1a7dbe85b39c6f96f58960ea61e5e
unknown <roberts2561@gmail.com>
2020-02-12 19:05:19 -0800

    blaw

--- src/main/java/frc/robot/commands/PixyCommand.java
+++ src/main/java/frc/robot/commands/PixyCommand.java
@@ -1,7 +1,9 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
 import frc.robot.Robot;
+import io.github.pseudoresonance.pixy2api.Pixy2CCC.Block;
 
 public class PixyCommand extends CommandBase{
     public PixyCommand() {
@@ -16,13 +18,21 @@ public class PixyCommand extends CommandBase{
     // Called repeatedly when this Command is scheduled to run
     @Override
     public void execute() {
-        Robot.pixy.GotoClosest();
+        // Robot.pixy.GotoClosest();
+        TurnToClosest();
     }
 
     // Make this return true when this Command no longer needs to run execute()
     @Override
     public boolean isFinished() {
         return false;
+    }
+
+    void TurnToClosest(){
+        Block block = Robot.pixy.getClosest();
+        double angle = block.getAngle();
+        
+        Robot.driveSystem.arcadeDrive(0.0, Constants.maxRightTurnRate, angle);
     }
 
     // Called once after isFinished returns true
