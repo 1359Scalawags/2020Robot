4ac8ffc19afd1907df104c635b3c62d417bca298
LEBANON\tim.helland <tim.helland@lebanon.k12.or.us>
2020-02-14 20:29:53 -0800

    Updated with changes from Matthew.

--- src/main/java/frc/robot/commands/TurnLoaderToRejectBalls.java
+++ src/main/java/frc/robot/commands/TurnLoaderToRejectBalls.java
@@ -9,7 +9,7 @@ import frc.robot.Robot;
 public class TurnLoaderToRejectBalls extends CommandBase{
 
     public TurnLoaderToRejectBalls() {
-
+        addRequirements(Robot.loadingSystem);
     }
 
     @Override
@@ -17,12 +17,6 @@ public class TurnLoaderToRejectBalls extends CommandBase{
         Robot.loadingSystem.setLoadInMotors(-0.8);
         Robot.loadingSystem.setLoadUpMotors(0.0);
         Robot.loadingSystem.setLoadInMotors(0.0);
-    }
-
-    // Called repeatedly when this Command is scheduled to run
-    @Override
-    public void execute() {
-
     }
 
     // Make this return true when this Command no longer needs to run execute()
@@ -34,8 +28,6 @@ public class TurnLoaderToRejectBalls extends CommandBase{
     // Called once after isFinished returns true
     @Override
     public void end(boolean interrupted) {
-        Robot.loadingSystem.setLoadInMotors(0.0);
-        Robot.loadingSystem.setLoadUpMotors(0.0);
-        Robot.loadingSystem.setChamberLoadMotor(0.0);
+
     }
 }
