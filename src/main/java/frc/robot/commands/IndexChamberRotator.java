93699e15a03464214010ebf41693338bc1db75ca
strutton1476 <strutton1476@lebanon.k12.or.us>
2020-02-14 20:04:51 -0800

    index sensor should work

--- src/main/java/frc/robot/commands/IndexChamberRotator.java
+++ src/main/java/frc/robot/commands/IndexChamberRotator.java
@@ -7,9 +7,29 @@ import frc.robot.Robot;
 public class IndexChamberRotator extends CommandBase{
     public IndexChamberRotator(){
         addRequirements(Robot.loadingSystem);
+    }
 
-
+    @Override
+    public void initialize() {
         Robot.loadingSystem.rotateChamber(Constants.maxChamberSpeed);
     }
+
+    // Called repeatedly when this Command is scheduled to run
+    @Override
+    public void execute() {
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    @Override
+    public void end(boolean interrupted) {
+    }
+
+    
 
 }
