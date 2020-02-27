20e10cc169eb8ecd42db6d79a65ed15e2b49e1df
Deadender-2005 <jasmer1702@lebanon.k12.or.us>
2020-02-26 19:22:11 -0800

    Add Drive Station simulation config.

--- src/main/java/frc/robot/commands/drive/AutoDriveTurn.java
+++ src/main/java/frc/robot/commands/drive/AutoDriveTurn.java
@@ -27,7 +27,11 @@ public class AutoDriveTurn extends CommandBase {
     private double currentangle;
 
 
+<<<<<<< HEAD
     public AutoDriveTurn(double angle) { 
+=======
+    public AutoDriveTurn(float angle) { 
+>>>>>>> Add Drive Station simulation config.
          addRequirements(Robot.driveSystem);
     //right is positive, left is negative
         targetangle = angle;
