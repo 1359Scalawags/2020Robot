f55f433c8afd624f445876f812b6ec931207ee92
LEBANON\hellandtester <tim.helland@lebanon.k12.or.us>
2020-02-26 20:04:14 -0800

    added the command for the loadindex

--- src/main/java/frc/robot/commands/load/IndexChamberRotator.java
+++ src/main/java/frc/robot/commands/load/IndexChamberRotator.java
@@ -2,22 +2,44 @@ package frc.robot.commands.load;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants.Load;
+import frc.robot.subsystems.LoadingSystem;
 import frc.robot.Robot;
+import edu.wpi.first.wpilibj.Timer;
 
 public class IndexChamberRotator extends CommandBase{
+    private Timer indexerDelay;
+
     public IndexChamberRotator(){
         addRequirements(Robot.loadingSystem);
+        indexerDelay = new Timer();
+        
     }
 
     @Override
     public void initialize() {
+        indexerDelay.reset();
+        indexerDelay.start();
         Robot.loadingSystem.rotateChamber(Load.maxChamberSpeed);
+    }
+
+    @Override
+    public void execute() {
+
+
     }
 
     // Make this return true when this Command no longer needs to run execute()
     @Override
     public boolean isFinished() {
-        return Robot.loadingSystem.getIndex();
+        if(indexerDelay.get() > Load.indextimer)
+        {
+            if(Robot.loadingSystem.isAtIndex()) {
+                return true;
+            }           
+            else{
+                return false;
+            }
+        }
     }
 
     // Called once after isFinished returns true
