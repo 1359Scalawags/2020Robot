573773a3db79569eef0cad69a3412bb6a8b3611b
LEBANON\hellandtester <tim.helland@lebanon.k12.or.us>
2020-02-29 15:06:53 -0800

    fixed a bug in robot.java. add chiald for a can works in the climbsystem

--- src/main/java/frc/robot/helper/CanMotor.java
+++ src/main/java/frc/robot/helper/CanMotor.java
@@ -5,7 +5,9 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.ControlType;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.Sendable;
 import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.smartdashboard.SendableBuilder;
 
 import com.revrobotics.CANPIDController;
 
@@ -13,7 +15,7 @@ import com.revrobotics.CANPIDController;
 import frc.robot.Constants;
 import frc.robot.helper.PID_Values;
 
-public class CanMotor implements SpeedController{
+public class CanMotor implements SpeedController, Sendable {
     private CANSparkMax motor;
     private double value;
     private PID_Values pid;
@@ -21,13 +23,11 @@ public class CanMotor implements SpeedController{
     private ControlType controlType;
     private CANPIDController controller;
     private CANEncoder encoder;
-    
-// SpeedController speedController, SpeedController[] speedControllers 
 
+    // SpeedController speedController, SpeedController[] speedControllers
 
-
-    public CanMotor(int id){
-        this(id, Constants.MAXRPM, ControlType.kVelocity ,new PID_Values());
+    public CanMotor(int id) {
+        this(id, Constants.MAXRPM, ControlType.kVelocity, new PID_Values());
     }
 
     public CanMotor(int id, double maxScaleValue, ControlType controlType, PID_Values pid) {
@@ -39,55 +39,57 @@ public class CanMotor implements SpeedController{
         this.motor.setInverted(false);
         this.controller = this.motor.getPIDController();
         this.controller.setOutputRange(-1, 1);
-        this.encoder = this.motor.getEncoder(); 
+        this.encoder = this.motor.getEncoder();
         updatePID();
     }
 
-    public double getSpeed(){
+    public double getSpeed() {
         return value;
     }
 
-
-    private void updatePID(){
-        double[] pids = {controller.getP(), controller.getI(), controller.getD(), controller.getIZone(), controller.getFF()};
-        if(!pid.equals(pids)){
+    private void updatePID() {
+        double[] pids = { controller.getP(), controller.getI(), controller.getD(), controller.getIZone(),
+                controller.getFF() };
+        if (!pid.equals(pids)) {
             pid = new PID_Values(pids);
         }
     }
-    public PID_Values getPID(){
-       return pid;
+
+    public PID_Values getPID() {
+        return pid;
     }
 
     /**
      * Sets velocity as percentage of maximum RPM.
+     * 
      * @param value_ A value between -1 and 1
      */
     private void setValue(double value_, boolean forcedUpdate) {
-        if(value != value_ || forcedUpdate){
+        if (value != value_ || forcedUpdate) {
             value = value_;
             updateSpeed();
         }
     }
 
-    public void updateSpeed(){
-        controller.setReference(value*Constants.MAXRPM, ControlType.kVelocity);
+    public void updateSpeed() {
+        controller.setReference(value * Constants.MAXRPM, ControlType.kVelocity);
     }
 
-    public CANEncoder Encoder(){
+    public CANEncoder Encoder() {
         return this.encoder;
     }
     // public SpeedController SpeedCont(){
-    //     return speedCont;
+    // return speedCont;
     // }
 
-    public CANSparkMax Motor(){
+    public CANSparkMax Motor() {
         return motor;
     }
 
-    public CANPIDController Controller(){
+    public CANPIDController Controller() {
         return controller;
     }
-    
+
     /**
      * @return The current value of the motor between -1 and 1.
      */
@@ -98,25 +100,28 @@ public class CanMotor implements SpeedController{
 
     /**
      * Sets percentage value for the motor dependent on the current ControlType.
+     * 
      * @param value A value between -1 and 1.
      */
     @Override
     public void set(double value) {
-        this.setValue(value, false);
+        this.setValue(value, true);
     }
 
-    public void setPID(double[] arr){
+    public void setPID(double[] arr) {
         pid = new PID_Values(arr);
     }
 
     /**
      * Sets percentage value for the motor dependent on the current ControlType.
-     * @param value A value between -1 and 1 that is multipled by the Scale.
-     * @param forcedupdate Update the underlying motor object even if the value has not changed.
+     * 
+     * @param value        A value between -1 and 1 that is multipled by the Scale.
+     * @param forcedupdate Update the underlying motor object even if the value has
+     *                     not changed.
      * @see BrushlessCANMotor#getScale() BrushlessCANMotor.getScale()
-     */ 
+     */
     public void set(double value, boolean forcedupdate) {
-        if(value != this.value || forcedupdate) {
+        if (value != this.value || forcedupdate) {
             this.value = value;
             updateMotor();
         }
@@ -152,5 +157,14 @@ public class CanMotor implements SpeedController{
     public void pidWrite(double output) {
         this.motor.pidWrite(output);
 
+    }
+
+    @Override
+    public void initSendable(SendableBuilder builder) {
+        // TODO Auto-generated method stub
+        builder.setSmartDashboardType("Speed Controller");
+        builder.setActuator(true);
+        builder.setSafeState(this::disable);
+        builder.addDoubleProperty("Value", this::get, this::set);
     }
 }
