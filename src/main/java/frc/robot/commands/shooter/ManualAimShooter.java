fc781b0ccc4e38b0edba1c3282e6b07d1b43e9e4
sandberg1503 <sandberg1503@lebanon.k12.or.us>
2020-02-29 16:25:16 -0800

    Updating TODOs

--- src/main/java/frc/robot/commands/shooter/ManualAimShooter.java
+++ src/main/java/frc/robot/commands/shooter/ManualAimShooter.java
@@ -3,6 +3,9 @@ package frc.robot.commands.shooter;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Robot;
 
+//TODO Make sure this gets finished for allowing moving up/down, left/right,
+//TODO and having limits for each
+
 public class ManualAimShooter extends CommandBase{
 
     public ManualAimShooter() {
