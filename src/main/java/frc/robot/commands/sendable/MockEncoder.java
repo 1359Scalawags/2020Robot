63fb48855b591910dd8745536c734b7d4e56bc2a
LEBANON\hellandtester <tim.helland@lebanon.k12.or.us>
2020-03-01 14:17:51 -0800

    added  sendebal mockencoder

---
+++ src/main/java/frc/robot/commands/sendable/MockEncoder.java
@@ -0,0 +1,53 @@
+package frc.robot.sendable;
+
+import edu.wpi.first.wpilibj.smartdashboard.SendableBuilder;
+
+/**
+ * An {@link IEncoder} that can be controlled via the dashboard.
+ */
+public class MockEncoder implements IEncoder {
+
+    private double distance;
+    private double rate;
+
+    @Override
+    public void reset() {
+        this.distance = 0;
+    }
+
+    @Override
+    public double getDistance() {
+        return this.distance;
+    }
+
+    /**
+     * Set the current distance the "encoder" has traveled.
+     * 
+     * @param distance the "encoder" has traveled
+     */
+    public void setDistance(final double distance) {
+        this.distance = distance;
+    }
+
+    @Override
+    public double getRate() {
+        return this.rate;
+    }
+
+    /**
+     * Sets the current rate of the "encoder" movement.
+     * 
+     * @param rate the "encoder" is moving at
+     */
+    public void setRate(final double rate) {
+        this.rate = rate;
+    }
+
+    @Override
+    public void initSendable(final SendableBuilder builder) {
+        builder.setSmartDashboardType("Quadrature Encoder");
+        builder.addDoubleProperty("Speed", this::getRate, this::setRate);
+        builder.addDoubleProperty("Distance", this::getDistance, this::setDistance);
+        builder.addDoubleProperty("Distance per Tick", () -> 0, null);
+    }
+}
