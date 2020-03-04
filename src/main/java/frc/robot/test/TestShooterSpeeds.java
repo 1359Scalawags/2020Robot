bef4cfc355fac2473c6f4eb8a35e60dc4a78249c
unknown <roberts2561@gmail.com>
2020-03-03 18:48:40 -0800

    Now it's back to test hopefully

--- src/main/java/frc/robot/test/TestShooterSpeeds.java
+++ src/main/java/frc/robot/test/TestShooterSpeeds.java
@@ -1,4 +1,4 @@
-package frc.robot.ttlest;
+package frc.robot.test;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Robot;
