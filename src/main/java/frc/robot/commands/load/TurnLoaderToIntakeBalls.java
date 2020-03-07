b4080a761b5bd20f43ea0b500258cb9d39b033d0
LEBANON\hellandtester <tim.helland@lebanon.k12.or.us>
2020-03-07 15:58:24 -0800

    added a timer and other funtions to turnLoaderToIntake

--- src/main/java/frc/robot/commands/load/TurnLoaderToIntakeBalls.java
+++ src/main/java/frc/robot/commands/load/TurnLoaderToIntakeBalls.java
@@ -6,32 +6,46 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Robot;
 //import frc.robot.RobotMap;
 import frc.robot.Constants.Load;
+import edu.wpi.first.wpilibj.Timer;
 
 public class TurnLoaderToIntakeBalls extends CommandBase{
 
     //TODO: will need a timer for when sensor is tripped
+    
+    private double start;
+    private Timer timer;
 
     public TurnLoaderToIntakeBalls() {
         addRequirements(Robot.loadingSystem);
+        timer = new Timer();
     }
 
     @Override
     public void initialize() {
+        timer.reset();
         // TODO: check if a ball is already in loading slot before starting   
         Robot.loadingSystem.setLoadInMotors(Load.loadIn);
-        
+        Robot.loadingSystem.getBallSlots();
     }
 
     @Override 
     public void execute() {
         // TODO: when ball is sensed in loader, start a timer
-        
+       
+        if (Robot.loadingSystem.isBallPreloading() && timer.get() == 0){
+            timer.start();
+            start = timer.get();
+        }
     }
-
     @Override
     public boolean isFinished() {
         // TODO: is finished when timer is done
-        return true;
+        if(timer.get() - start > Load.currentTime){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
     @Override
