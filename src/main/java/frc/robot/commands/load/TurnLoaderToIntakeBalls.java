2e0f65bd4d7558d1dd738985e315d64e805ee8e4
strutton1476 <strutton1476@lebanon.k12.or.us>
2020-03-20 14:18:07 -0700

    worked on todo's in loader intake command

--- src/main/java/frc/robot/commands/load/TurnLoaderToIntakeBalls.java
+++ src/main/java/frc/robot/commands/load/TurnLoaderToIntakeBalls.java
@@ -9,17 +9,17 @@ import frc.robot.Constants.Load;
 import edu.wpi.first.wpilibj.Timer;
 
 public class TurnLoaderToIntakeBalls extends CommandBase{    
-    private double start;
-    private Timer timer;
+    // private double start;
+    // private Timer timer;
 
     public TurnLoaderToIntakeBalls() {
         addRequirements(Robot.loadingSystem);
-        timer = new Timer();
+        // timer = new Timer();
     }
 
     @Override
     public void initialize() {
-        timer.reset();
+        // timer.reset();
         // TODO: check if a ball is already in loading slot before starting   
         Robot.loadingSystem.setLoadInMotors(Load.loadIn);
         Robot.loadingSystem.getBallSlots();
@@ -27,16 +27,13 @@ public class TurnLoaderToIntakeBalls extends CommandBase{
 
     @Override 
     public void execute() {
-        // TODO: when ball is sensed in loader, start a timer
-       
-        if (Robot.loadingSystem.isBallPreloading() && timer.get() == 0){
-            timer.start();
-            start = timer.get();
-        }
+        // if (Robot.loadingSystem.isBallPreloading() && timer.get() == 0){
+        //     timer.start();
+        //     start = timer.get();
+        // }
     }
     @Override
     public boolean isFinished() {
-        // TODO: is finished when timer is done
         // if(timer.get()-start > Load.timerMaxTime) {
         //     return true;
         // }
