749b4f7e7ae860ddbb16744d74ec90d798929503
unknown <roberts2561@gmail.com>
2021-04-28 19:50:41 -0700

    added a testing folder

---
+++ src/test/java/LoadingSystemTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+
+import edu.wpi.first.hal.HAL;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.simulation.DoubleSolenoidSim;
+import edu.wpi.first.wpilibj.simulation.PWMSim;
+import frc.robot.Constants.IntakeConstants;
+import org.junit.*;
+
+public class LoadingSystemTest {
+    public static final double DELTA = 1e-2; // acceptable deviation range
+    
+}
